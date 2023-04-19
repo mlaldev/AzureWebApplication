2e33bd903d1d7d5e0828696a6d2c59a2b9911c0f
mlaldev <mlal10111997@gmail.com>
2023-04-19 20:50:01 +0530

    Project

--- src/main/java/com/azure/server/ServerApplication.java
+++ src/main/java/com/azure/server/ServerApplication.java
@@ -47,7 +47,7 @@ public class ServerApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://ecommerceapplicationbatch6.azurewebsites.net"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
